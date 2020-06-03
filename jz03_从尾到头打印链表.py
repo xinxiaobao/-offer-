e83b0bc10676b35272f5fbd2c3e6d7f2e211843c
xinxiaobao <18030179713@163.com>
2020-06-03 11:22:53 +0800

    add jz03

---
+++ jz03_从尾到头打印链表.py
@@ -0,0 +1,16 @@
+# 算法逻辑： 把所以值取出来，放到列表中， 最好把列表倒过来，即可
+
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # 返回从尾部到头部的列表值序列，例如[1,2,3]
+    def printListFromTailToHead(self, listNode):
+        # write code here
+        res = []
+        while listNode:
+            res.append(listNode.val)
+            listNode = listNode.next
+        return res[::-1]
