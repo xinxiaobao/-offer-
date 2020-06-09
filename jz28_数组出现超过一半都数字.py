00c7d053426f4b83ea34979a68aa89c9ea8cfbc5
xinxiaobao <18030179713@163.com>
2020-06-09 13:07:12 +0800

    add the algorithm of jz28

--- jz28_数组出现超过一半都数字.py
+++ jz28_数组出现超过一半都数字.py
@@ -1,7 +1,5 @@
-
-
-
-
+# 算法思路： 统计数组每个数字出现频率，组成字典； 取字典的值最大，如果超过数组长度一半
+#           则返回该字典的值，否则返回0
 
 class Solution:
     def MoreThanHalfNum_Solution(self, numbers):
@@ -15,7 +13,7 @@ class Solution:
         dic_max = max(dic.values())
         dic_max_key = list(dic.keys())[list(dic.values()).index(dic_max)]
         return dic_max_key if 2*dic_max > len(numbers) else 0
-        
+
 numbers = [4,2,1,4,2,4]
 sol = Solution()
 print(sol.MoreThanHalfNum_Solution(numbers))
